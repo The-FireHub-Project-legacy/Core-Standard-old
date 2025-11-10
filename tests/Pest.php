7f6bb54bc101450ab6f67d8fb2c32da4a94ebadc
Riddick <danijel.galic@live.com>
2025-11-10 14:13:26 +0100

    Try Pest

---
+++ tests/Pest.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of the FireHub Web Application Framework package
+ *
+ * @author Danijel Galić <danijel.galic@outlook.com>
+ * @copyright 2025 FireHub Web Application Framework
+ * @license <https://opensource.org/licenses/OSL-3.0> OSL Open Source License version 3
+ *
+ * @package Core\Test
+ *
+ * @version GIT: $Id$ Blob checksum.
+ */
+
+namespace FireHub\Tests;
+
+pest()
+    ->group('smoke')
+    ->in('smoke');
+
+pest()
+    ->group('unit-lowlevel')
+    ->in('unit\support\lowlevel');
+
+pest()
+    ->group('unit-components')
+    ->in('unit\support\lowlevel');
