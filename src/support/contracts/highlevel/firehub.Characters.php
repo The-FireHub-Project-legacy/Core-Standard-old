dee73f2306288a00a47624003635fb36fd601a8f
Riddick <danijel.galic@live.com>
2025-12-11 08:44:52 +0100

    Create Characters high-level contract(#54)

---
+++ src/support/contracts/highlevel/firehub.Characters.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of the FireHub Web Application Framework package
+ *
+ * @author Danijel Galić <danijel.galic@outlook.com>
+ * @copyright 2025 FireHub Web Application Framework
+ * @license <https://opensource.org/licenses/OSL-3.0> OSL Open Source License version 3
+ *
+ * @php-version 7.4
+ * @package Core\Support
+ *
+ * @version GIT: $Id$ Blob checksum.
+ */
+
+namespace FireHub\Core\Support\Contracts\HighLevel;
+
+use FireHub\Core\Support\Contracts\Magic\Stringable;
+
+/**
+ * ### Characters contract
+ *
+ * Interface allows you to represent character.
+ * @since 1.0.0
+ *
+ * @template TCharacter of non-empty-string
+ */
+interface Characters extends Stringable {
+
+    /**
+     * @inheritDoc
+     *
+     * @since 1.0.0
+     *
+     * @return TCharacter The string representation of the object.
+     */
+    public function __toString ():string;
+
+}
